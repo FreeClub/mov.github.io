82db03c86adab45dee9b5784c571b9aed717a9e5
do985 <42400813+do985@users.noreply.github.com>
2022-12-13 23:51:29 +0800

    Add files via upload

--- js/data.js
+++ js/data.js
@@ -1,1 +1,32 @@
-
+var dataList =
+    [
+        {
+            url: "https://jx.xiaolangyun.com/?url=",
+            name: "线路1"
+        },
+        {
+            url: "https://api.jiexi.la/?url=",
+            name: "线路2"
+        },
+        {
+            url: "https://www.8090g.cn/?url=",
+            name: "线路3"
+        },
+        {
+            url: "https://www.ckplayer.vip/jiexi/?url=",
+            name: "线路4"
+        },
+        {
+            url: "https://www.pangujiexi.cc/jiexi.php?url=",
+            name: "线路5"
+        }
+        ,
+        {
+            url: "https://www.h8jx.com/jiexi.php?url=",
+            name: "线路6"
+        },
+        {
+            url: "https://www.playm3u8.cn/jiexi.php?url=",
+            name: "线路7"
+        }
+    ];
