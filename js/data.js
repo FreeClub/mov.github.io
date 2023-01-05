fdb4cdcbc215d9c1cd7b1283fe35a1ab4b6e9449
do985 <42400813+do985@users.noreply.github.com>
2023-01-05 22:32:04 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -2,27 +2,27 @@ var dataList =
     [
         {
             url: "https://api.jiexi.la/?url=",
-            name: "线路2"
+            name: "线路1"
         },
         {
             url: "https://www.8090g.cn/?url=",
-            name: "线路3"
+            name: "线路2"
         },
         {
             url: "https://www.ckplayer.vip/jiexi/?url=",
-            name: "线路4"
+            name: "线路3"
         },
         {
             url: "https://www.pangujiexi.cc/jiexi.php?url=",
-            name: "线路5"
+            name: "线路4"
         }
         ,
         {
             url: "https://www.h8jx.com/jiexi.php?url=",
-            name: "线路6"
+            name: "线路5"
         },
         {
             url: "https://www.playm3u8.cn/jiexi.php?url=",
-            name: "线路7"
+            name: "线路6"
         }
     ];
