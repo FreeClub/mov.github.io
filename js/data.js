9e9823c41b6e812d1337bfd408d84a3917beae49
Joy <42400813+FreeClub@users.noreply.github.com>
2024-11-21 22:55:36 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -3,5 +3,9 @@ var dataList =
         {
             url: "https://www.8090g.cn/?url=",
             name: "线路1"
+        },
+        {
+            url: "https://jx.xmflv.com/?url=",
+            name: "线路1"
         }
     ];
